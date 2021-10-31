e66d1fb928b2d9188acaec1c7d808732e5385a10
Ruyangchen <3226717170@qq.com>
2021-10-31 19:39:14 +0800

    新闻写进txt

--- news_spider_scrapy/news_spider_scrapy/spiders/tencent_news_details.py
+++ news_spider_scrapy/news_spider_scrapy/spiders/tencent_news_details.py
@@ -7,15 +7,15 @@ from ..items import NewsDetailItem
 redis中新闻url的消费者。消费完后redis中无数据.
 """
 
+result_news_path = '/Users/foiunclekay/Documents/GitHub/news_spider_scrapy/news_spider_scrapy' \
+                   '/news_spider_scrapy/result_news/'
+
 
 class TencentNewsDetail(RedisSpider):
     name = 'TencentNewsDetail'
     redis_key = 'tencent_detail_news_url'
     allowed_domains = ['qq.com']
-
-    mobile_url = 'https://xw.qq.com/cmsid/{}'
-    mobile_ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38' \
-                ' (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+    count = 0
 
     custom_settings = {
         'CONCURRENT_REQUESTS': 64,
@@ -42,10 +42,6 @@ class TencentNewsDetail(RedisSpider):
         yield Request(
             url=response.url,
             callback=self.parse_detail,
-            headers={
-                'User-Agent': self.mobile_ua,
-                'Referer': response.url
-            }
         )
 
     def parse_detail(self, response):
@@ -72,5 +68,14 @@ class TencentNewsDetail(RedisSpider):
             article = soup.find_all('p', class_='one-p')
             for sentence in article:
                 news['content'] += sentence.text
+            self.count += 1
+            path = result_news_path + str(self.count) + '.txt'
+            print(path)
+            with open(path, 'w') as f:
+                f.write(news['url'])
+                f.write('\n')
+                f.write(news['title'])
+                f.write('\n')
+                f.write(news['content'])
         except Exception as e:
             self.logger.error('parse_content error {}, {}, {}'.format(e, response.url, response.text))
